45180b50752460c77a2cda96ccaf46876c2570aa
tannerheimsch <h3imscht@outlook.com>
2023-03-23 21:22:52 -0500

    README test

--- Database Import/pizza_shop.sql
+++ Database Import/pizza_shop.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80030 (8.0.30)
  File Encoding         : 65001
 
- Date: 22/03/2023 00:10:55
+ Date: 22/03/2023 02:40:15
 */
 
 SET NAMES utf8mb4;
@@ -882,7 +882,7 @@ CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `staffcost` AS select `r`
 -- View structure for stock1
 -- ----------------------------
 DROP VIEW IF EXISTS `stock1`;
-CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `stock1` AS select `s1`.`item_name` AS `item_name`,`s1`.`ing_id` AS `ing_id`,`s1`.`ing_name` AS `ing_name`,`s1`.`ing_weight` AS `ing_weight`,`s1`.`ing_price` AS `ing_price`,`s1`.`Order_Quantity` AS `order_quantity`,`s1`.`Recipe_Quantity` AS `recipe_quantity`,(`s1`.`Order_Quantity` * `s1`.`Recipe_Quantity`) AS `Ordered_Weight`,(`s1`.`ing_price` / `s1`.`ing_weight`) AS `Unit_Cost`,round(((`s1`.`Order_Quantity` * `s1`.`Recipe_Quantity`) * (`s1`.`ing_price` / `s1`.`ing_weight`)),2) AS `Ingredient_Cost` from (select `o`.`item_id` AS `item_id`,`i`.`sku` AS `sku`,`i`.`item_name` AS `item_name`,`r`.`ing_id` AS `ing_id`,`ing`.`ing_name` AS `ing_name`,`r`.`quantity` AS `Recipe_Quantity`,sum(`o`.`quantity`) AS `Order_Quantity`,`ing`.`ing_weight` AS `ing_weight`,`ing`.`ing_price` AS `ing_price` from (((`orders` `o` left join `item` `i` on((`o`.`item_id` = `i`.`item_id`))) left join `recipe` `r` on((`i`.`sku` = `r`.`recipe_id`))) left join `ingredient` `ing` on((`ing`.`ing_id` = `r`.`ing_id`))) group by `o`.`item_id`,`i`.`sku`,`i`.`item_name`,`r`.`ing_id`,`r`.`quantity`,`ing`.`ing_name`,`ing`.`ing_weight`,`ing`.`ing_price`) `s1`;
+CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `stock1` AS select concat(`s1`.`item_cat`,' ',`s1`.`item_name`,' ',`s1`.`item_size`) AS `ItemFullName`,`s1`.`ing_id` AS `ing_id`,`s1`.`ing_name` AS `ing_name`,`s1`.`ing_weight` AS `ing_weight`,`s1`.`ing_price` AS `ing_price`,`s1`.`Order_Quantity` AS `order_quantity`,`s1`.`Recipe_Quantity` AS `recipe_quantity`,(`s1`.`Order_Quantity` * `s1`.`Recipe_Quantity`) AS `Ordered_Weight`,(`s1`.`ing_price` / `s1`.`ing_weight`) AS `Unit_Cost`,round(((`s1`.`Order_Quantity` * `s1`.`Recipe_Quantity`) * (`s1`.`ing_price` / `s1`.`ing_weight`)),2) AS `Ingredient_Cost` from (select `o`.`item_id` AS `item_id`,`i`.`sku` AS `sku`,`i`.`item_name` AS `item_name`,`i`.`item_cat` AS `item_cat`,`i`.`item_size` AS `item_size`,`r`.`ing_id` AS `ing_id`,`ing`.`ing_name` AS `ing_name`,`r`.`quantity` AS `Recipe_Quantity`,sum(`o`.`quantity`) AS `Order_Quantity`,`ing`.`ing_weight` AS `ing_weight`,`ing`.`ing_price` AS `ing_price` from (((`orders` `o` left join `item` `i` on((`o`.`item_id` = `i`.`item_id`))) left join `recipe` `r` on((`i`.`sku` = `r`.`recipe_id`))) left join `ingredient` `ing` on((`ing`.`ing_id` = `r`.`ing_id`))) group by `o`.`item_id`,`i`.`sku`,`i`.`item_name`,`i`.`item_cat`,`i`.`item_size`,`r`.`ing_id`,`r`.`quantity`,`ing`.`ing_name`,`ing`.`ing_weight`,`ing`.`ing_price`) `s1`;
 
 -- ----------------------------
 -- View structure for stock2
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# pizza-shop-project
-My first extensive database project where I create a relational database, come up with custom SQL queries, and then populate that data into a dashboard with various reports.
+# Pizza Shop Data Dashboard 🍕
+> In this data implementation project, I created a relational database from scratch to fulfill the needs of a hypothetical pizza company looking to take a deeper look into their order trends, inventory levels, and staff costs.
+
+---
+
+## Introduction
+
+By combining manual record creation in Excel, mock data generated from [Mockaroo](https://www.mockaroo.com/) using schema data, as well as the Data Generation Tool in Navicat - I was able to create a functional database and build a dashboard via Google's Looker Studio based off custom queries.
+
+---
